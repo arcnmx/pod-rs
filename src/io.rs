8d79c73d7d8e1ba4561c926b876b9d7ce39e9fd3
arcnmx <arcnmx@users.noreply.github.com>
2016-07-11 15:53:49 -0400

    Remove endian, add io extension traits

---
+++ src/io.rs
@@ -0,0 +1,55 @@
+use std::io;
+use pod::Pod;
+
+#[cfg(feature = "uninitialized")]
+use uninitialized::uninitialized;
+#[cfg(not(feature = "uninitialized"))]
+use std::mem::zeroed as uninitialized;
+
+#[cfg(feature = "read_exact")]
+use read_exact::ReadExactExt;
+
+/// An extension trait for reading `Pod` types from `std::io::Read` data streams.
+pub trait PodReadExt {
+    /// Reads a `Pod` struct from the stream. Behaves like `read_exact`, and will
+    /// produce an error if EOF is encountered before the data is fully read.
+    fn read_pod<P: Pod>(&mut self) -> io::Result<P>;
+
+    /// Reads a `Pod` struct from the stream, or nothing at EOF. Partial reads
+    /// will result in an error.
+    #[cfg(feature = "read_exact")]
+    fn read_pod_or_none<P: Pod>(&mut self) -> io::Result<Option<P>>;
+}
+
+impl<T: io::Read> PodReadExt for T {
+    fn read_pod<P: Pod>(&mut self) -> io::Result<P> {
+        let mut data: P = unsafe { uninitialized() };
+
+        self.read_exact(data.as_mut_bytes()).map(|_| data)
+    }
+
+    #[cfg(feature = "read_exact")]
+    fn read_pod_or_none<P: Pod>(&mut self) -> io::Result<Option<P>> {
+        let mut data: P = unsafe { uninitialized() };
+
+        self.read_exact_or_eof(data.as_mut_bytes()).map(|read| if read {
+            Some(data)
+        } else {
+            None
+        })
+    }
+}
+
+/// An extension trait for writing `Pod` types to `std::io::Write` data streams.
+pub trait PodWriteExt {
+    /// Writes the memory representation of a `Pod` struct to the stream.
+    /// Behaves like `write_all`, failure to write the entire structure will
+    /// result in an error.
+    fn write_pod<P: Pod>(&mut self, data: &P) -> io::Result<()>;
+}
+
+impl<T: io::Write> PodWriteExt for T {
+    fn write_pod<P: Pod>(&mut self, data: &P) -> io::Result<()> {
+        self.write_all(data.as_bytes())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,47 +5,17 @@
 //! # Safety
 //!
 //! The `nue-macros` crate can be used for safe automagic derives.
-//!
-//! # Example
-//!
-//! ```
-//! use pod::{Pod, Le, Be};
-//! # #[cfg(not(feature = "packed/oibit"))]
-//! # mod stable {
-//! # use pod::packed::{Unaligned, Packed};
-//! # unsafe impl Packed for super::Data { }
-//! # unsafe impl Unaligned for super::Data { }
-//! # }
-//!
-//! unsafe impl Pod for Data { }
-//!
-//! #[repr(C)]
-//! struct Data(u8, Le<u16>, Be<u32>);
-//!
-//! # fn main() {
-//! let data = Data(1, Le::new(0x2055), Be::new(0xdeadbeef));
-//!
-//! let cmp = &[
-//!     0x01,
-//!     0x55, 0x20,
-//!     0xde, 0xad, 0xbe, 0xef,
-//! ];
-//!
-//! assert_eq!(cmp, data.as_bytes());
-//! # }
-//!
-//! ```
 
-extern crate byteorder;
+#[cfg(feature = "uninitialized")]
 extern crate uninitialized;
+#[cfg(feature = "read_exact")]
+extern crate read_exact;
 
 /// Re-export the `packed` crate
 pub extern crate packed;
 
-/// Containers for primitives
-pub mod endian;
-
 mod pod;
+mod io;
 
-pub use endian::{Le, Be, Native};
 pub use pod::Pod;
+pub use io::{PodReadExt, PodWriteExt};
